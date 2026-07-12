68727fad1609c72d9bece3d1cf151c19ce015d23
giulioserrato-lgtm <giulioserrato@gmail.com>
2026-07-11 21:04:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cuidadores-cache-v1';
+const CACHE_NAME = 'cuidadores-cache-v2';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -24,8 +24,14 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cached) => cached || caches.match('./index.html'))
+      )
   );
 });
